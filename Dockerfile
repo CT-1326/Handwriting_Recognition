d7cea61541abfa2974b09502eb6a217b273fa26f
CT-1326 <33346331+CT-1326@users.noreply.github.com>
2021-12-07 15:51:00 +0900

    mnist 인식 모델 구현

--- Dockerfile
+++ Dockerfile
@@ -2,10 +2,10 @@ FROM tensorflow/tensorflow:2.5.0
 
 WORKDIR /usr/src
 
-# COPY ./requirements.txt .
+COPY ./requirements.txt .
 
 RUN pip install --no-cache-dir --upgrade pip
-# RUN pip install -r requirements.txt
+RUN pip install -r requirements.txt
 
 COPY . .
 
--- index.py
+++ index.py
@@ -25,3 +25,26 @@ model.fit(x_train, y_train, epochs=5)
 # 6. 정확도 평가
 test_loss, test_acc = model.evaluate(x_test, y_test)
 print('테스트 정확도:', test_acc)
+
+# 딥러닝에 필요한 케라스 함수 호출
+from keras.models import load_model
+from keras.utils import np_utils
+
+# 필요 라이브러리 호출(PIL은 이미지파일 처리위함)
+from PIL import Image
+import numpy as np
+
+# test.png는 그림판에서 붓으로 숫자 8을 그린 이미지 파일
+# test.png 파일 열어서 L(256단계 흑백이미지)로 변환
+img = Image.open("test.png").convert("L")
+
+# 이미지를 784개 흑백 픽셀로 사이즈 변환
+img = np.resize(img, (1, 784))
+
+# 데이터를 모델에 적용할 수 있도록 가공
+test_data = ((np.array(img) / 255) - 1) * -1
+
+# 클래스 예측 함수에 가공된 테스트 데이터 넣어 결과 도출
+res =(model.predict(test_data) > 0.5).astype("int32")
+
+print('인식된 숫자는 : ', res)
